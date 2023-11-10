659119ebfced6899b2cad004960adc895ee7bf12
yuliia-barabash <146951532+yuliia-barabash@users.noreply.github.com>
2023-11-10 10:52:05 +0100

    add frontend

---
+++ frontend/search-app-opensearch/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import TypeaheadSearch from "./components/searchBar";
+
+function App() {
+  return (
+    <div className='App'>
+        <TypeaheadSearch />
+    </div>
+  )
+}
+
+
+
+export default App;
